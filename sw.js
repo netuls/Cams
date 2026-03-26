0256d339de2afa2d5bd44b988c2e555bfc379c43
netuls <56983125+netuls@users.noreply.github.com>
2026-03-26 18:04:54 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,16 @@
-const CACHE = 'ce-treino-v1';
-const URLS  = ['./', './index.html', './style.css', './scripts.js', './firebase-config.js', './manifest.json'];
+const CACHE_NAME = 'cams-gym-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './scripts.js',
+    './firebase-config.js'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
